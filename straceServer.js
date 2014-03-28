0c942f3837dcd8d1987805069dd9e15455bd33b5
Andree Monette <andreecmonette@gmail.com>
2014-03-28 15:39:44 -0400

    New branch for node-based straceviz

--- straceServer.js
+++ straceServer.js
@@ -1,61 +1,11 @@
-/*
-var child_process = require('child_process');
-var readline = require('readline');
-
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  stderr: process.stderr
-});
-
-rl.question("strace ", function(answer) {
-  var spawnProc = answer.split(" ");
-  spawnProc.unshift("-ttt");
-  var echoProc = child_process.spawn("strace", spawnProc);
-  var lastLine = "";
-  echoProc.stderr.on('data', function (chunk) {
-    var lines = chunk.toString().split("\n");
-    lines[0] = lastLine + lines[0];
-    lastLine = lines.pop();
-    
-    for (line in lines) {
-      var matchArray = lines[line].match(/(\S+?) (\S+?)\((.*?)\) +\= (.*)$/)
-      if (matchArray) {
-      sysCalls.push({
-        
-        time : matchArray[1],
-        call : matchArray[2],
-        args : matchArray[3].split(", "),
-        retVal : matchArray[4]
-      });
-      } else {
-        sysCalls.push({
-          call : lines[line]
-        });
-      }
-    }
-   
-
-    
-  console.log(sysCalls);
-  });
-  rl.close();
-
-});
-*/
-
-
-
-
 var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app, {log : false})
+  , io = require('socket.io').listen(app)//, {log : false})
   , fs = require('fs')
   , child_process = require('child_process');
 
 app.listen(3000);
 
 function handler (req, res) {
-  //console.log(req);
   fs.readFile(__dirname + "/straceGraph.html",
   function (err, data) {
     if (err) {
@@ -96,6 +46,7 @@ io.sockets.on('connection', function (socket) {
           retVal : matchArray[4]
         });
         } else {
+          // errors
           //sysCalls.push({
           //  call : lines[line]
           //});
