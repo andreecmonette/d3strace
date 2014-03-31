50a89afcb11f5a7ffe7247dd21a3d76648bf5fec
Damian Kao <Damian@Damians-MacBook-Pro.local>
2014-03-31 17:07:00 -0400

    ui

--- straceServer.js
+++ straceServer.js
@@ -1,10 +1,13 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
-  , child_process = require('child_process');
+  , child_process = require('child_process')
+  , os = require('os');
 
 app.listen(3000);
 
+var stop = false;
+var proces = [];
 function handler (req, res) {
   //console.log(req);
   fs.readFile(__dirname + "/straceGraph.html",
@@ -20,34 +23,61 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
+  var proc = null;
+
+  socket.on('stop', function(data) {
+    stop = true;
+  })
+
   var index = 0;
   socket.on('strace', function(data) {
     var call = data.val;
-    var spawnProc = call.split(" ");
-    spawnProc.unshift("dtruss");
 
-    console.log(spawnProc)
-    var echoProc = child_process.spawn("sudo", spawnProc);
-    var lastLine = "";
+    if (proc != null) {
+      proc.stdin.write(data);
+    } else {
+      var spawnProc = call.split(" ");
+      //osx
+      spawnProc.unshift('sudo -u damian')
+      spawnProc.unshift("dtruss");
+      var proc = child_process.spawn("sudo", spawnProc);
 
-    echoProc.stderr.on('data', function (chunk) {
-      var sysCalls = [];
-      var lines = chunk.toString().split("\n");
-      lines.pop();
-      lines[0] = lastLine + lines[0];
-      lastLine = lines.pop();
-      
-      for (line in lines) {
-        var call = lines[line].split('(')[0];
-        sysCalls.push({
-          call: call
-        });
-      }
-      socket.emit('sysCalls', {index:index++, content:sysCalls});
-    });
+      /*linux
+      spawnProc.unshift("-ttt");
+      var echoProc = child_process.spawn("strace", spawnProc);
+      */
 
-    echoProc.stdout.on('data', function(chunk) {
-      socket.emit('stdout', {content:chunk.toString().split('\n')})
-    })
+      var lastLine = "";
+
+      proc.stderr.on('data', function (chunk) {
+        if (stop == false) {
+          var sysCalls = [];
+          var lines = chunk.toString().split("\n");
+          lines.pop();
+          lines[0] = lastLine + lines[0];
+          lastLine = lines.pop();
+          
+          for (line in lines) {
+            var call = lines[line].split('(')[0];
+            sysCalls.push({
+              call: call
+            });
+          }
+          socket.emit('sysCalls', {index:index++, content:sysCalls});
+        } else {
+          proc.stderr.end();
+          stop = false;
+        }
+      });
+
+      proc.stdout.on('data', function(chunk) {
+        socket.emit('stdout', {content:chunk.toString().split('\n')})
+      })
+
+      proc.on('close', function() {
+        socket.emit('stdout', {content:'closed'});
+        proc = null;
+      })
+    }
   })
 });
